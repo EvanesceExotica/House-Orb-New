9efeeca87b5d5e4d93eb91705db9a6948a61ac9d
Eva <EvanesceExotica@gmail.com>
2018-07-22 14:20:51 -0400

    Added new assets along with a "Mute Pause" class to handle volume pauses for effect.

--- Assets/MutePause.cs
+++ Assets/MutePause.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MutePause : MonoBehaviour {
 
 	AudioListener audioListener;
 	// Use this for initialization
+
+	void LowerVolume(){
+		AudioListener.volume *= 0.5f;
+	}
+
+	void RaiseVolumeToNormal(){
+		AudioListener.volume *= 2;
+
+	}
+
+	public IEnumerator MutePauseVolume(){
+		LowerVolume();
+		yield return new WaitForSeconds(1.0f);
+		RaiseVolumeToNormal();
+
+	}
 	void Start () {
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetKeyDown(KeyCode.V)){
+			StartCoroutine(MutePauseVolume());
+		}
 		
 	}
 }
